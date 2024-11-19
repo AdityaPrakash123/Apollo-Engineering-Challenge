fc8af24e226e70bf2fbc1c9d1f98a48ba4ca1156
Aditya Bhagavatula <adityabp@hotmail.co.uk>
2024-11-19 02:16:31 -0600

    Added ApolloEngineeringChallenge.Tests project

---
+++ ApolloEngineeringChallenge.Tests/UnitTest1.cs
@@ -0,0 +1,18 @@
+using ApolloEngineeringChallenge.Controllers;
+using ApolloEngineeringChallenge.Models;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using Xunit;  // Add for test attributes
+
+namespace ApolloEngineeringChallenge.Tests
+{
+    public class UnitTest1
+    {
+        // [Fact]
+        // public void Test1()
+        //{
+
+        //}
+
+    }
+}
